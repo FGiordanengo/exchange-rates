d7899fa5d2980405e2a9d9ddb28162af2fe090eb
FGiordanengo <florent.giordanengo@gmail.com>
2020-01-14 12:28:54 +0100

    Processing conversion v1

---
+++ script.js
@@ -0,0 +1,27 @@
+var formElement = document.querySelector('.userInput form');
+formElement.addEventListener('submit', function(event) {
+    event.preventDefault();
+    //On récupère la valeur du formulaire
+    var valueElement = document.querySelector(".userInput input[name=inputValue]");
+    // console.log(valueElement.value);
+    //propriété .value permet de récupérer la valeur du sélecteur ciblé
+    var value = valueElement.value
+
+    var rate = 1.11;
+    result = value * rate;
+    // console.log(result);
+
+    var fromValueElement = document.querySelector(".result .fromValue");
+    var toValueElement = document.querySelector(".result .toValue");
+
+    function roundDecimal(nombre, precision){
+        var precision = precision || 2;
+        var tmp = Math.pow(10, precision);
+        return Math.round( nombre*tmp )/tmp;
+    }
+
+    fromValueElement.innerHTML = value + ' € ';
+    toValueElement.innerHTML = ' $' + roundDecimal(result, 2);
+
+    
+})
